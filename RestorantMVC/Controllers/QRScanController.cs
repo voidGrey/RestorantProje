47312d715063cf36c1de960d7b7527d3c5a7898a
Oğuz Taşdemir <oguzbtasdemir@gmail.com>
2023-08-16 14:25:01 +0300

    QRScannerController

---
+++ RestorantMVC/Controllers/QRScanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestorantMVC.Controllers
+{
+    public class QRScanController : Controller
+    {
+        public IActionResult Scan(string tableId)
+        {
+            return RedirectToAction("Index", "Home", new { tableId });
+        }
+    }
+}
