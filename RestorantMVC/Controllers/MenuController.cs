704a7872b895b04dd9bff7a2d6f324f20ae2a177
Serkan Özgürel <serkanozgurel@outlook.com>
2023-08-23 14:51:24 +0300

    menucontroller

---
+++ RestorantMVC/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestorantMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly SqlDbContext dbContext;
+
+        public MenuController(SqlDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        public async  Task<IActionResult> Kategori( int? id)
+        {
+            var urunler = dbContext.Urunler.Where(a => a.KategoriID == id).ToList();
+            return View();
+        }
+    }
+}
