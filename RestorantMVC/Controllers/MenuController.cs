cf36a0a0e7a825efe538d57f0daa273da8c10b56
Mustafa Ertargın <=>
2023-09-01 21:44:11 +0300

    MenuController Summary's

--- RestorantMVC/Controllers/MenuController.cs
+++ RestorantMVC/Controllers/MenuController.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace RestorantMVC.Controllers
 {
+    /// <summary>
+    /// Menü işlemlerini yönetmek için kullanılan Controller sınıfı.
+    /// </summary>
     public class MenuController : Controller
     {
         private readonly SqlDbContext dbContext;
@@ -14,12 +17,20 @@ namespace RestorantMVC.Controllers
             this.dbContext = dbContext;
         }
 
+        /// <summary>
+        /// Belirli bir kategoriye ait ürünleri listelemek için kullanılan işlem.
+        /// </summary>
+        /// <param name="id">Kategori kimliği</param>
         public async Task<IActionResult> Kategori(int? id)
         {
             var urunler = await dbContext.Urunler.Where(a => a.KategoriID == id).ToListAsync();
             return View(urunler);
         }
 
+        /// <summary>
+        /// Belirli bir ürünü siparişe eklemek için kullanılan GET işlemi.
+        /// </summary>
+        /// <param name="id">Eklenen ürünün kimliği</param>
         [HttpGet]
         public async Task<IActionResult> Ekle(int id)
         {
@@ -31,7 +42,7 @@ namespace RestorantMVC.Controllers
             //DB'De ki Sipariş Master'leri çekiyorum
             var masters = dbContext.SiparisMasterlar.Where(s => s.MasaId == masaid).Where(s => s.IsActive == true);
             // SiparişDetail atamak için yeni bir detail listesi oluşturuyorum.
-            List<SiparisDetay> details  = new();
+            List<SiparisDetay> details = new();
 
             //DB'den çekilen Sipariş Master' yeni siparisMaster olarak atıyorum
             SiparisMaster siparisMaster = masters.FirstOrDefault();
@@ -44,7 +55,7 @@ namespace RestorantMVC.Controllers
                 yeniSiparis = false;
             }
 
-            // DB'den gelen siparişMaster' null ise yeni bir tane oluşturuyorum.
+            // DB'den gelen siparisMaster' null ise yeni bir tane oluşturuyorum.
             if (siparisMaster == null)
             {
                 siparisMaster = new SiparisMaster();
@@ -62,14 +73,14 @@ namespace RestorantMVC.Controllers
 
             // Gelen ürün sipariş detay olarak oluşturuluyor.
             SiparisDetay siparisDetay = new SiparisDetay
-                (
-                siparisMaster.ID,
-                siparisMaster,
-                await dbContext.Urunler.FindAsync(id),
-                dbContext.Urunler.Find(id).ID,
-                1,
-                dbContext.Urunler.Find(id).Fiyat
-                );
+        (
+        siparisMaster.ID,
+        siparisMaster,
+        await dbContext.Urunler.FindAsync(id),
+        dbContext.Urunler.Find(id).ID,
+        1,
+        dbContext.Urunler.Find(id).Fiyat
+        );
 
             // Yeni eklenen ürün siparisMaster'de ki sipariş Detay listesine ekleniyor.
             siparisMaster.SiparisDetay.Add(siparisDetay);
@@ -84,16 +95,18 @@ namespace RestorantMVC.Controllers
 
             //Siparişler liste olarak sayfaya gönderiliyor.
             ICollection<SiparisDetay> siparisler = dbContext.SiparisDetaylar.Where(sd => sd.SiparisMaster.MasaId == siparisMaster.MasaId).ToList();
-            return RedirectToAction("Index","Siparis");
+            return RedirectToAction("Index" , "Siparis");
         }
 
-
-
-
-                [HttpPost]
+        /// <summary>
+        /// Belirli bir ürünü siparişe eklemek için kullanılan POST işlemi.
+        /// </summary>
+        /// <param name="siparisMaster">Sipariş Master nesnesi</param>
+        [HttpPost]
         public async Task<IActionResult> Ekle(SiparisMaster siparisMaster)
         {
             return View();
         }
     }
+
 }
