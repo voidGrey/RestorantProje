1e96714b1d26ba4470299915841f585626c12bd3
Mustafa Ertargın <voidgrey@gmail.com>
2023-09-21 16:06:29 +0300

    Login ve Register
    Login ve register Ekranları tasarlandı,
    Login ekranına özel Layout yapıldı
    Automapper' da kayıt olmayı engelleyen bir bug giderildi.

--- RestorantMVC/AutoMapperConfig/MyAutoMapper.cs
+++ RestorantMVC/AutoMapperConfig/MyAutoMapper.cs
@@ -1,14 +1,16 @@
 ﻿using AutoMapper;
+using Elfie.Serialization;
 using Entites.Concrate;
 using RestorantMVC.Models;
 
 namespace RestorantMVC.AutoMapperConfig
 {
+
     public class MyAutoMapper :Profile
     {
         public MyAutoMapper()
         {
-            CreateMap<FirmaRegisterDTO , Firma>();
+            CreateMap<FirmaRegisterDTO , Firma>().ForMember(f => f.UserName , o => o.MapFrom(src => src.FirmaAdi));
         }
     }
 }
---
+++ RestorantMVC/Views/FirmaLogin/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestorantMVC.Views.FirmaLogin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
